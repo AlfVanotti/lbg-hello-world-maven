59f2003a2b1fff7261736b9d66c96f33b5af34fa
Alf V <alfvanotti@hotmail.com>
2024-12-03 13:58:05 +0000

    updated

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -11,7 +11,7 @@ public class App
         System.out.println(sayHello());
         System.out.println(sayHelloToSomeone("Alice"));
         System.out.println(sayHelloToSomeone("Robin"));
-        System.out.println(sayHelloToSomeone("Batman"));
+        System.out.println(sayHelloToSomeone("Batmann"));
         System.out.println(sayHelloToSomeone("Alf"));
         System.out.println(sayGoodbye());
     }
